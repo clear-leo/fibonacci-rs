eb724e32753c9e73ee4a4b7ddf65f97343d1b1ae
clearleo <g567zp@gmail.com>
2024-07-26 15:29:57 -0500

    Add all files

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+
+fn main() {
+    let mut times = String::new();
+    io::stdin().read_line(&mut times);
+    let times: f32 = times.trim().parse().expect("Please enter a number!"); 
+
+    println!("{}", fibonacci(times));
+}
+
+fn fibonacci(mut n:f32) -> f32 {
+    let phi: f32 = (1. + 5_f32.sqrt())/2.;
+    n = n - 1.0;
+    return ((phi.powf(n) - (-phi).powf(-n)) / (5_f32).sqrt()).round();
+}
